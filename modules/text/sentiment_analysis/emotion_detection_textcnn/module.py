6b8fe4094d5fe2c6ddf80882c6afa505cba2ea2a
zhangxuefei <zhangxuefei@baidu.com>
2020-03-02 11:22:58 +0800

    add emotion_detection_textcnn

---
+++ modules/text/sentiment_analysis/emotion_detection_textcnn/module.py
@@ -0,0 +1,320 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import ast
+import json
+import numpy as np
+import os
+import six
+
+import paddle.fluid as fluid
+from paddle.fluid.core import PaddleDType, PaddleTensor, AnalysisConfig, create_paddle_predictor
+import paddlehub as hub
+from paddlehub.common.utils import sys_stdin_encoding
+from paddlehub.io.parser import txt_parser
+from paddlehub.module.module import moduleinfo
+from paddlehub.module.module import runnable
+
+import sys
+sys.path.append("..")
+
+from emotion_detection_textcnn.net import textcnn_net
+from emotion_detection_textcnn.processor import load_vocab, preprocess, postprocess
+
+import time
+
+
+class DataFormatError(Exception):
+    def __init__(self, *args):
+        self.args = args
+
+
+@moduleinfo(
+    name="emotion_detection_textcnn",
+    version="1.1.0",
+    summary="Baidu's open-source Emotion Detection Model(TextCNN).",
+    author="baidu-nlp",
+    author_email="paddle-dev@baidu.com",
+    type="nlp/sentiment_analysis")
+class EmotionDetectionTextCNN(hub.Module):
+    def _initialize(self, ):
+        """
+        initialize with the necessary elements
+        """
+        self.pretrained_model_path = os.path.join(self.directory, "model")
+        self.vocab_path = os.path.join(self.directory, "assets/vocab.txt")
+        self.vocab = load_vocab(self.vocab_path)
+        self.lac = None
+
+        self._set_config()
+
+    def _set_config(self, ):
+        """
+        predictor config setting
+        """
+        self.pretrained_model_path = os.path.join(self.directory, "infer_model")
+        cpu_config = AnalysisConfig(self.pretrained_model_path)
+        cpu_config.disable_glog_info()
+        cpu_config.disable_gpu()
+        self.cpu_predictor = create_paddle_predictor(cpu_config)
+
+        try:
+            _places = os.environ["CUDA_VISIBLE_DEVICES"]
+            int(_places[0])
+            use_gpu = True
+        except:
+            use_gpu = False
+        if use_gpu:
+            gpu_config = AnalysisConfig(self.pretrained_model_path)
+            gpu_config.disable_glog_info()
+            gpu_config.enable_use_gpu(memory_pool_init_size_mb=500, device_id=0)
+            self.gpu_predictor = create_paddle_predictor(gpu_config)
+
+    def context(
+            self,
+            trainable=False,
+    ):
+        """
+        Get the input ,output and program of the pretrained emotion_detection_textcnn
+        Args:
+             trainable(bool): whether fine-tune the pretrained parameters of emotion_detection_textcnn or not
+        Returns:
+             inputs(dict): the input variables of emotion_detection_textcnn (words)
+             outputs(dict): the output variables of emotion_detection_textcnn (the sentiment prediction results)
+             main_program(Program): the main_program of lac with pretrained prameters
+        """
+        main_program = fluid.Program()
+        startup_program = fluid.Program()
+        with fluid.program_guard(main_program, startup_program):
+            with fluid.unique_name.guard("@HUB_emotion_detection_textcnn@"):
+                # word seq data
+                data = fluid.layers.data(
+                    name="words", shape=[1], dtype="int64", lod_level=1)
+                # label data
+                label = fluid.layers.data(
+                    name="label", shape=[1], dtype="int64")
+
+                cost, acc, prediction, fc = textcnn_net(data, label, 240465)
+
+                for param in main_program.global_block().iter_parameters():
+                    param.trainable = trainable
+
+                place = fluid.CPUPlace()
+                exe = fluid.Executor(place)
+
+                # load the emotion_detection_textcnn pretrained model
+                def if_exist(var):
+                    return os.path.exists(
+                        os.path.join(self.pretrained_model_path, var.name))
+
+                fluid.io.load_vars(
+                    exe, self.pretrained_model_path, predicate=if_exist)
+
+                inputs = {"words": data}
+                outputs = {"class_probs": prediction, "sentence_feature": fc}
+
+                return inputs, outputs, main_program
+
+    def texts2tensor(self, texts):
+        """
+        Tranform the texts(dict) to PaddleTensor
+        Args:
+             texts(dict): texts
+        Returns:
+             tensor(PaddleTensor): tensor with texts data
+        """
+        lod = [0]
+        data = []
+        for i, text in enumerate(texts):
+            data += text['processed']
+            lod.append(len(text['processed']) + lod[i])
+        tensor = PaddleTensor(np.array(data).astype('int64'))
+        tensor.name = "words"
+        tensor.lod = [lod]
+        tensor.shape = [lod[-1], 1]
+        return tensor
+
+    def to_unicode(self, texts):
+        """
+        Convert each element's type(str) of texts(list) to unicode in python2.7
+        Args:
+             texts(list): each element's type is str in python2.7
+        Returns:
+             texts(list): each element's type is unicode in python2.7
+        """
+
+        if six.PY2:
+            unicode_texts = []
+            for text in texts:
+                if not isinstance(text, unicode):
+                    unicode_texts.append(
+                        text.decode(sys_stdin_encoding()).decode("utf8"))
+                else:
+                    unicode_texts.append(text)
+            texts = unicode_texts
+        return texts
+
+    def emotion_classify(self, texts=[], data={}, use_gpu=False, batch_size=1):
+        """
+        Get the emotion prediction results results with the texts as input
+        Args:
+             texts(list): the input texts to be predicted, if texts not data
+             data(dict): key must be 'text', value is the texts to be predicted, if data not texts
+             use_gpu(bool): whether use gpu to predict or not
+             batch_size(int): the program deals once with one batch
+        Returns:
+             results(dict): the emotion prediction results
+        """
+        try:
+            _places = os.environ["CUDA_VISIBLE_DEVICES"]
+            int(_places[0])
+        except:
+            use_gpu = False
+
+        if texts != [] and isinstance(texts, list) and data == {}:
+            predicted_data = texts
+        elif texts == [] and isinstance(data, dict) and isinstance(
+                data.get('text', None), list) and data['text']:
+            predicted_data = data["text"]
+        else:
+            raise ValueError(
+                "The input data is inconsistent with expectations.")
+
+        predicted_data = self.to_unicode(predicted_data)
+        if not self.lac:
+            self.lac = hub.Module(
+                directory="/ssd2/home/zhangxuefei/.paddlehub/modules/lac")
+        processed_results = preprocess(self.lac, predicted_data, self.vocab,
+                                       use_gpu)
+        tensor_words = self.texts2tensor(processed_results)
+
+        if use_gpu:
+            fetch_out = self.gpu_predictor.run([tensor_words])
+        else:
+            fetch_out = self.cpu_predictor.run([tensor_words])
+        result = postprocess(fetch_out[0], processed_results)
+        return result
+
+    @runnable
+    def run_cmd(self, argvs):
+        """
+        Run as a command
+        """
+        self.parser = argparse.ArgumentParser(
+            description="Run the emotion_detection_textcnn module.",
+            prog='hub run emotion_detection_textcnn',
+            usage='%(prog)s',
+            add_help=True)
+
+        self.arg_input_group = self.parser.add_argument_group(
+            title="Input options", description="Input data. Required")
+        self.arg_config_group = self.parser.add_argument_group(
+            title="Config options",
+            description=
+            "Run configuration for controlling module behavior, not required.")
+
+        self.add_module_config_arg()
+        self.add_module_input_arg()
+
+        args = self.parser.parse_args(argvs)
+
+        try:
+            input_data = self.check_input_data(args)
+        except DataFormatError and RuntimeError:
+            self.parser.print_help()
+            return None
+
+        results = self.sentiment_classify(
+            texts=input_data, use_gpu=args.use_gpu, batch_size=args.batch_size)
+        if six.PY2:
+            try:
+                results = json.dumps(
+                    results, encoding="utf8", ensure_ascii=False)
+            except:
+                pass
+
+        return results
+
+    def add_module_config_arg(self):
+        """
+        Add the command config options
+        """
+        self.arg_config_group.add_argument(
+            '--use_gpu',
+            type=ast.literal_eval,
+            default=False,
+            help="whether use GPU for prediction")
+
+        self.arg_config_group.add_argument(
+            '--batch_size',
+            type=int,
+            default=1,
+            help="batch size for prediction")
+
+    def add_module_input_arg(self):
+        """
+        Add the command input options
+        """
+        self.arg_input_group.add_argument(
+            '--input_file',
+            type=str,
+            default=None,
+            help="file contain input data")
+        self.arg_input_group.add_argument(
+            '--input_text', type=str, default=None, help="text to predict")
+
+    def check_input_data(self, args):
+        input_data = []
+        if args.input_file:
+            if not os.path.exists(args.input_file):
+                print("File %s is not exist." % args.input_file)
+                raise RuntimeError
+            else:
+                input_data = txt_parser.parse(args.input_file, use_strip=True)
+        elif args.input_text:
+            if args.input_text.strip() != '':
+                if six.PY2:
+                    input_data = [
+                        args.input_text.decode(
+                            sys_stdin_encoding()).decode("utf8")
+                    ]
+                else:
+                    input_data = [args.input_text]
+            else:
+                print(
+                    "ERROR: The input data is inconsistent with expectations.")
+
+        if input_data == []:
+            print("ERROR: The input data is inconsistent with expectations.")
+            raise DataFormatError
+
+        return input_data
+
+    def get_vocab_path(self, ):
+        """
+        Get the path to the vocabulary whih was used to pretrain
+        Returns:
+             self.vocab_path(str): the path to vocabulary
+        """
+        return self.vocab_path
+
+
+if __name__ == "__main__":
+    emotion_detection_textcnn = EmotionDetectionTextCNN()
+    emotion_detection_textcnn = hub.Module(name='emotion_detection_textcnn')
+    # Data to be predicted
+    test_text = ["今天天气真好", "湿纸巾是干垃圾", "别来吵我"]
+
+    input_dict = {"text": test_text}
+    results = emotion_detection_textcnn.emotion_classify(
+        data=input_dict, batch_size=10)
+    for result in results:
+        print(result['text'])
+        print(result['emotion_label'])
+        print(result['emotion_key'])
+        print(result['positive_probs'])
+        print(result['negative_probs'])
+        print(result['neutral_probs'])
