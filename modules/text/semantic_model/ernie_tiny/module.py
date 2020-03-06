8decaccbcbd1d51d57d283b04569183b0000cfe1
kinghuin <kinghuin_chull@163.com>
2020-03-06 17:43:08 +0800

    add ernie_tiny

---
+++ modules/text/semantic_model/ernie_tiny/module.py
@@ -0,0 +1,74 @@
+#coding:utf-8
+# Copyright (c) 2019  PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from paddlehub import BERTModule
+from paddlehub.module.module import moduleinfo
+
+from ernie_tiny.model.ernie import ErnieModel, ErnieConfig
+
+
+@moduleinfo(
+    name="ernie_tiny",
+    version="1.1.0",
+    summary=
+    "Baidu's ERNIE-tiny, Enhanced Representation through kNowledge IntEgration, tiny version, max_seq_len=512",
+    author="baidu-nlp",
+    author_email="nlp@baidu.com",
+    type="nlp/semantic_model",
+)
+class EnieTiny(BERTModule):
+    def _initialize(self):
+        ernie_config_path = os.path.join(self.directory,
+                                         "ernie_tiny_config.json")
+        self.ernie_config = ErnieConfig(ernie_config_path)
+        self.MAX_SEQ_LEN = 512
+        self.params_path = os.path.join(self.directory, "assets", "params")
+        self.vocab_path = os.path.join(self.directory, "assets", "vocab.txt")
+        self.spm_path = os.path.join(self.directory, "assets", "subword",
+                                     "spm_cased_simp_sampled.model")
+        self.word_dict_path = os.path.join(self.directory, "assets", "subword",
+                                           "dict.wordseg.pickle")
+
+    def net(self,
+            input_ids,
+            position_ids,
+            segment_ids,
+            input_mask,
+            task_ids=None):
+        if not task_ids:
+            self.ernie_config._config_dict['use_task_id'] = False
+        ernie = ErnieModel(
+            src_ids=input_ids,
+            position_ids=position_ids,
+            sentence_ids=segment_ids,
+            task_ids=task_ids,
+            input_mask=input_mask,
+            config=self.ernie_config,
+            use_fp16=False)
+        pooled_output = ernie.get_pooled_output()
+        sequence_output = ernie.get_sequence_output()
+        return pooled_output, sequence_output
+
+
+if __name__ == '__main__':
+    test_module = EnieTiny()
