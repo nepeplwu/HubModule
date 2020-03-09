e69bc0c743832aadab4c7fb0f7032d8598044b44
kinghuin <kinghuin_chull@163.com>
2020-03-09 11:03:00 +0800

    add ernie 2 base

---
+++ modules/text/semantic_model/ernie_tiny/download.sh
@@ -0,0 +1,3 @@
+wget https://ernie.bj.bcebos.com/ernie_tiny.tar.gz
+tar xzvf ernie_tiny.tar.gz
+mv ernie_tiny assets
---
+++ modules/text/semantic_model/ernie_v2_eng_base/download.sh
@@ -0,0 +1,3 @@
+wget https://ernie.bj.bcebos.com/ERNIE_Base_en_stable-2.0.0.tar.gz
+tar xzvf ERNIE_Base_en_stable-2.0.0.tar.gz
+mv ERNIE_Base_en_stable-2.0.0 assets
