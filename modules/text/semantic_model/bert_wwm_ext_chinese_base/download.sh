2004015a6b02d25dbff6c75350ebd9875ee7f4cd
kinghuin <kinghuin_chull@163.com>
2020-03-10 20:39:21 +0800

    add bert_wwm_ext_chinese_base

---
+++ modules/text/semantic_model/bert_wwm_ext_chinese_base/download.sh
@@ -0,0 +1,3 @@
+wget https://paddlehub.bj.bcebos.com/model/nlp/chinese_wwm_ext_L-12_H-768_A-12_fluid.tar.gz --no-check-certificate
+tar xzvf chinese_wwm_ext_L-12_H-768_A-12_fluid.tar.gz
+mv chinese_wwm_ext_L-12_H-768_A-12_fluid assets
