659c16ebb7bb52662dd14941dc1f6755b139050e
kinghuin <kinghuin_chull@163.com>
2020-03-10 10:38:28 +0800

    add bert multi

---
+++ modules/text/semantic_model/bert/download.sh
@@ -0,0 +1,3 @@
+wget https://bert-models.bj.bcebos.com/multi_cased_L-12_H-768_A-12.tar.gz --no-check-certificate
+tar multi_cased_L-12_H-768_A-12.zip
+mv multi_cased_L-12_H-768_A-12 assets
--- modules/text/semantic_model/ernie_tiny/download.sh
+++ modules/text/semantic_model/ernie_tiny/download.sh
@@ -1,3 +1,3 @@
-wget https://ernie.bj.bcebos.com/ernie_tiny.tar.gz
+wget https://ernie.bj.bcebos.com/ernie_tiny.tar.gz --no-check-certificate
 tar xzvf ernie_tiny.tar.gz
 mv ernie_tiny assets
