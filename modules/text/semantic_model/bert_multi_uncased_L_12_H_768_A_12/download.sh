d8f73fa90a32e79a52974466f2ca87d3384251a0
kinghuin <kinghuin_chull@163.com>
2020-03-12 15:18:29 +0800

    fix bert multi uncased

--- modules/text/semantic_model/bert_multi_uncased_L_12_H_768_A_12/download.sh
+++ modules/text/semantic_model/bert_multi_uncased_L_12_H_768_A_12/download.sh
@@ -1,12 +1,8 @@
-project_path=$PWD
-rawname=${project_path##*/}
-name=${rawname//L_12_H_768_A_12/L-12_H-768_A-12}
-name=${name//L_24_H_1024_A_16/L-24_H-1024_A-16}
+wget https://bert-models.bj.bcebos.com/multilingual_L-12_H-768_A-12.tar.gz --no-check-certificate
+tar xvf multilingual_L-12_H-768_A-12.tar.gz
+mv multilingual_L-12_H-768_A-12 assets
 
-python /qjx/PaddleHub/paddlehub/commands/hub.py install $name
-mv ~/.paddlehub/modules/$rawname/assets .
-mv ~/.paddlehub/modules/$rawname/model assets/params
-rm -r ~/.paddlehub/modules/$rawname
-python /qjx/PaddleHub/paddlehub/commands/hub.py install ../$rawname
+cd ..
+python /qjx/PaddleHub/paddlehub/commands/hub.py install bert_multi_uncased_L_12_H_768_A_12
+cd bert_multi_uncased_L_12_H_768_A_12
 python test_embedding.py
-rm -r ~/.paddlehub/modules/$rawname
--- modules/text/semantic_model/roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation/download.sh
+++ modules/text/semantic_model/roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation/download.sh
@@ -2,5 +2,7 @@ wget https://paddlehub.bj.bcebos.com/model/nlp/chinese_rbtl3_L-3_H-1024_A-16_flu
 tar xzvf chinese_rbtl3_L-3_H-1024_A-16_fluid.tar.gz
 mv chinese_rbtl3_L-3_H-1024_A-16_fluid assets
 
-python /qjx/PaddleHub/paddlehub/commands/hub.py install ../roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation
+cd ..
+python /qjx/PaddleHub/paddlehub/commands/hub.py install roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation
+cd roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation
 python test_embedding.py
