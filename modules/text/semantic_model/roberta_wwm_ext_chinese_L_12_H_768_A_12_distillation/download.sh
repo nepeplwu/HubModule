3794f619966266b722ad6b30badcf890312385d2
kinghuin <kinghuin_chull@163.com>
2020-03-12 14:20:54 +0800

    fit shell

--- modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
+++ modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
@@ -1,10 +1,10 @@
 project_path=$PWD
-name=${project_path##*/}
-name=${name//L_12_H_768_A_12/L-12_H-768_A-12}
+rawname=${project_path##*/}
+name=${rawname//L_12_H_768_A_12/L-12_H-768_A-12}
 name=${name//L_24_H_1024_A_16/L-24_H-1024_A-16}
 
-hub install $name
-mv ~/.paddlehub/modules/$name/assets .
-mv ~/.paddlehub/modules/$name/model assets/params
-rm -r ~/.paddlehub/modules/$name
-hub install ../$name
+python /qjx/PaddleHub/paddlehub/commands/hub.py install $name
+mv ~/.paddlehub/modules/$rawname/assets .
+mv ~/.paddlehub/modules/$rawname/model assets/params
+rm -r ~/.paddlehub/modules/$rawname
+python /qjx/PaddleHub/paddlehub/commands/hub.py install ../$rawname
