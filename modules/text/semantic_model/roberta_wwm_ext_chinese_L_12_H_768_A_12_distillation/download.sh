81af324e4b1b2550ef32049ecdb29b66f70d5d63
kinghuin <kinghuin_chull@163.com>
2020-03-12 14:56:36 +0800

    fix distillation

--- modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
+++ modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
@@ -1,12 +1,6 @@
-project_path=$PWD
-rawname=${project_path##*/}
-name=${rawname//L_12_H_768_A_12/L-12_H-768_A-12}
-name=${name//L_24_H_1024_A_16/L-24_H-1024_A-16}
+wget https://paddlehub.bj.bcebos.com/model/nlp/chinese_rbt3_L-3_H-768_A-12_fluid.tar.gz --no-check-certificate
+tar xzvf chinese_rbt3_L-3_H-768_A-12_fluid.tar.gz
+mv chinese_rbt3_L-3_H-768_A-12_fluid assets
 
-python /qjx/PaddleHub/paddlehub/commands/hub.py install $name
-mv ~/.paddlehub/modules/$rawname/assets .
-mv ~/.paddlehub/modules/$rawname/model assets/params
-rm -r ~/.paddlehub/modules/$rawname
-python /qjx/PaddleHub/paddlehub/commands/hub.py install ../$rawname
+python /qjx/PaddleHub/paddlehub/commands/hub.py install ../roberta_wwm_ext_chinese_L_24_H_1024_A_16_distillation
 python test_embedding.py
-rm -r ~/.paddlehub/modules/$rawname
