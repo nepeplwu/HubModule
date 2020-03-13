be8a7142703860dc4f1df3c179eb323f9dc7ab00
chengjuntao <chengjuntao@baidu.com>
2020-03-13 06:06:16 +0000

    rm save origin image

--- modules/image/object_detection/faster_rcnn/processor.py
+++ modules/image/object_detection/faster_rcnn/processor.py
@@ -127,8 +127,6 @@ def postprocess(paths,
             org_img_path = get_save_image_name(
                 org_img, output_dir, 'image_numpy_{}.jpg'.format(
                     (handle_id + index)))
-            if visualization:
-                org_img.save(org_img_path)
 
         org_img_height = org_img.height
         org_img_width = org_img.width
