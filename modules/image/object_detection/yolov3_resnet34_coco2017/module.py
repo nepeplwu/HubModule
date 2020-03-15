21a77c32852c8865928460f9bd13d1f9acc08af8
chengjuntao <chengjuntao@baidu.com>
2020-03-15 12:31:29 +0000

    add run_cmd for yolov3

--- modules/image/object_detection/yolov3_resnet34_coco2017/module.py
+++ modules/image/object_detection/yolov3_resnet34_coco2017/module.py
@@ -4,12 +4,14 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import ast
+import argparse
 from functools import partial
 
 import numpy as np
 import paddle.fluid as fluid
 import paddlehub as hub
-from paddlehub.module.module import moduleinfo
+from paddlehub.module.module import moduleinfo, runnable
 from paddle.fluid.core import PaddleTensor, AnalysisConfig, create_paddle_predictor
 
 
@@ -169,11 +171,6 @@ class YOLOv3ResNet34(hub.Module):
         res = []
         for iter_id, feed_data in enumerate(batch_reader()):
             feed_data = np.array(feed_data)
-            #            data_out = exe.run(
-            #                self.infer_prog,
-            #                feed=feeder.feed(feed_data),
-            #                fetch_list=[self.bbox_out],
-            #                return_numpy=False)
             image_ = np.array(feeder.feed(feed_data)['image'])
             im_size = np.array(feeder.feed(feed_data)['im_size'])
             image_tensor = PaddleTensor(image_.copy())
@@ -196,3 +193,61 @@ class YOLOv3ResNet34(hub.Module):
 
             res.append(output)
         return res
+
+    def add_module_config_arg(self):
+        """
+        Add the command config options
+        """
+        self.arg_config_group.add_argument(
+            '--use_gpu',
+            type=ast.literal_eval,
+            default=False,
+            help="whether use GPU or not")
+
+        self.arg_config_group.add_argument(
+            '--batch_size',
+            type=int,
+            default=1,
+            help="batch size for prediction")
+        self.arg_config_group.add_argument(
+            '--user_dict',
+            type=str,
+            default=None,
+            help=
+            "customized dictionary for intervening the word segmentation result"
+        )
+
+    def add_module_input_arg(self):
+        """
+        Add the command input options
+        """
+        self.arg_input_group.add_argument(
+            '--input_path',
+            type=str,
+            default=None,
+            help="file contain input data")
+
+    @runnable
+    def run_cmd(self, argvs):
+        self.parser = argparse.ArgumentParser(
+            description="Run the .",
+            prog='hub run vgg16_imagenet',
+            usage='%(prog)s',
+            add_help=True)
+        self.arg_input_group = self.parser.add_argument_group(
+            title="Input options", description="Input data. Required")
+        self.arg_config_group = self.parser.add_argument_group(
+            title="Config options",
+            description=
+            "Run configuration for controlling module behavior, not required.")
+        self.add_module_config_arg()
+
+        self.add_module_input_arg()
+        args = self.parser.parse_args(argvs)
+        input_path = args.input_path
+        if os.path.exists(input_path) == False:
+            raise ValueError("input_path is not exit")
+        return self.object_detection(
+            paths=[input_path],
+            use_gpu=args.use_gpu,
+            batch_size=args.batch_size)
