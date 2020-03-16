cb2024b93d6170498bd48d1367a08075d0ab4429
chengjuntao <chengjuntao@baidu.com>
2020-03-16 03:27:51 +0000

    add iput_file in run_cmd

--- modules/image/classification/vgg16_imagenet/module.py
+++ modules/image/classification/vgg16_imagenet/module.py
@@ -8,6 +8,7 @@ import paddlehub as hub
 import paddle.fluid as fluid
 from paddlehub.module.module import moduleinfo, runnable
 from paddle.fluid.core import PaddleTensor, AnalysisConfig, create_paddle_predictor
+from paddlehub.io.parser import txt_parser
 
 from vgg16_imagenet.vgg import VGG
 from vgg16_imagenet.processor import load_label_info
@@ -222,6 +223,28 @@ class VGG16(hub.Module):
             default=None,
             help="file contain input data")
 
+        self.arg_input_group.add_argument(
+            '--input_file',
+            type=str,
+            default=None,
+            help="file contain input data")
+
+    def check_input_data(self, args):
+        input_data = []
+        if args.input_path:
+            if not os.path.exists(args.input_path):
+                print("File %s is not exist." % args.input_path)
+                raise RuntimeError
+            else:
+                input_data = [args.input_path]
+        elif args.input_file:
+            if not os.path.exists(args.input_file):
+                print("File %s is not exist." % args.input_file)
+                raise RuntimeError
+            else:
+                input_data = txt_parser.parse(args.input_file, use_strip=True)
+        return input_data
+
     @runnable
     def run_cmd(self, argvs):
         self.parser = argparse.ArgumentParser(
@@ -239,10 +262,6 @@ class VGG16(hub.Module):
 
         self.add_module_input_arg()
         args = self.parser.parse_args(argvs)
-        input_path = args.input_path
-        if os.path.exists(input_path) == False:
-            raise ValueError("input_path is not exit")
+        input_data = self.check_input_data(args)
         return self.classification(
-            paths=[input_path],
-            use_gpu=args.use_gpu,
-            batch_size=args.batch_size)
+            paths=input_data, use_gpu=args.use_gpu, batch_size=args.batch_size)
