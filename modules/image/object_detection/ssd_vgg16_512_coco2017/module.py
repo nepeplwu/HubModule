c388a7927b7bc0cae32568f9853d5d938a2de793
wuzewu <wuzewu@baidu.com>
2020-04-02 15:00:44 +0800

    Update ssd

--- modules/image/object_detection/ssd_vgg16_512_coco2017/module.py
+++ modules/image/object_detection/ssd_vgg16_512_coco2017/module.py
@@ -14,6 +14,7 @@ import paddlehub as hub
 from paddlehub.module.module import moduleinfo, runnable
 from paddle.fluid.core import PaddleTensor, AnalysisConfig, create_paddle_predictor
 from paddlehub.io.parser import txt_parser
+import yaml
 
 from ssd_vgg16_512_coco2017.vgg import VGG
 
@@ -37,6 +38,7 @@ class SSDVGG16(hub.Module):
         self.image = None
         self.bbox_out = None
         self._set_config()
+        self._config = None
 
     def _set_config(self):
         """
@@ -61,27 +63,19 @@ class SSDVGG16(hub.Module):
             self.gpu_predictor = create_paddle_predictor(gpu_config)
 
     def context(self,
-                multi_box_head=None,
-                ssd_output_decoder=None,
                 input_image=None,
                 trainable=True,
                 pretrained=True,
-                param_prefix='',
+                num_classes=81,
                 get_prediction=False):
         """Distill the Head Features, so as to perform transfer learning.
 
-        :param multi_box_head: SSD head of MultiBoxHead.
-        :type multi_box_head: <class 'MultiBoxHead' object>
-        :param ssd_output_decoder: SSD output decoder
-        :type ssd_output_decoder: <class 'SSDOutputDecoder' object>
         :param input_image: image tensor.
         :type input_image: <class 'paddle.fluid.framework.Variable'>
         :param trainable: whether to set parameters trainable.
         :type trainable: bool
         :param pretrained: whether to load default pretrained model.
         :type pretrained: bool
-        :param param_prefix: the prefix of parameters in multi_box_head and backbone
-        :type param_prefix: str
         :param get_prediction: whether to get prediction,
             if True, outputs is {'bbox_out': bbox_out},
             if False, outputs is {'head_features': head_features}.
@@ -106,44 +100,16 @@ class SSDVGG16(hub.Module):
                                           3], [128, 256, 1, 2, 3],
                                          [128, 256, 1, 1, 4]])
                 body_feats = backbone(image)
-
-                # multi_box_head
-                if multi_box_head is None:
-                    multi_box_head = self.ssd.MultiBoxHead(
-                        base_size=512,
-                        num_classes=81,
-                        aspect_ratios=[[2.], [2., 3.], [2., 3.], [2., 3.],
-                                       [2., 3.], [2.], [2.]],
-                        min_ratio=15,
-                        max_ratio=90,
-                        min_sizes=[
-                            20.0, 51.0, 133.0, 215.0, 296.0, 378.0, 460.0
-                        ],
-                        max_sizes=[
-                            51.0, 133.0, 215.0, 296.0, 378.0, 460.0, 542.0
-                        ],
-                        steps=[8, 16, 32, 64, 128, 256, 512],
-                        offset=0.5,
-                        flip=True,
-                        kernel_size=3,
-                        pad=1)
-                # ssd_output_decoder
-                if ssd_output_decoder is None:
-                    ssd_output_decoder = self.ssd.SSDOutputDecoder(
-                        nms_threshold=0.45,
-                        nms_top_k=400,
-                        keep_top_k=200,
-                        score_threshold=0.01,
-                        nms_eta=1.0,
-                        background_label=0)
-
+                # call ssd.context
                 inputs, outputs, context_prog = self.ssd.context(
                     body_feats=body_feats,
-                    multi_box_head=multi_box_head,
-                    ssd_output_decoder=ssd_output_decoder,
+                    multi_box_head=self.ssd.MultiBoxHead(
+                        num_classes=num_classes, **self.multi_box_head),
+                    ssd_output_decoder=self.ssd.SSDOutputDecoder(
+                        **self.output_decoder),
                     image=image,
                     trainable=trainable,
-                    param_prefix=param_prefix,
+                    var_prefix='@HUB_{}@'.format(self.name),
                     get_prediction=get_prediction)
 
                 place = fluid.CPUPlace()
@@ -155,20 +121,28 @@ class SSDVGG16(hub.Module):
                             os.path.join(self.default_pretrained_model_path,
                                          var.name))
 
-                    load_default_pretrained_model = True
-                    if param_prefix:
-                        load_default_pretrained_model = False
-                    elif input_image:
-                        if input_image.shape != (-1, 3, 512, 512):
-                            load_default_pretrained_model = False
-                    if load_default_pretrained_model:
-                        fluid.io.load_vars(
-                            exe,
-                            self.default_pretrained_model_path,
-                            predicate=_if_exist)
+                    fluid.io.load_vars(
+                        exe,
+                        self.default_pretrained_model_path,
+                        predicate=_if_exist)
                 else:
                     exe.run(startup_program)
                 return inputs, outputs, context_prog
+
+    @property
+    def config(self):
+        if not self._config:
+            with open(os.path.join(self.directory, 'config.yml')) as file:
+                self._config = yaml.load(file.read(), Loader=yaml.FullLoader)
+        return self._config
+
+    @property
+    def multi_box_head(self):
+        return self.config['MultiBoxHead']
+
+    @property
+    def output_decoder(self):
+        return self.config['SSDOutputDecoder']
 
     def object_detection(self,
                          paths=None,
