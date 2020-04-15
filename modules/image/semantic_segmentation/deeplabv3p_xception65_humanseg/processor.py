d22410f3adc6001ea49ce780f56756f2543f7cf9
linshuliang <xiaolin_2292@qq.com>
2020-04-15 03:22:34 +0000

    add function `base64_to_cv2` in processor.py

--- modules/image/semantic_segmentation/deeplabv3p_xception65_humanseg/processor.py
+++ modules/image/semantic_segmentation/deeplabv3p_xception65_humanseg/processor.py
@@ -7,10 +7,18 @@ import os
 import time
 from collections import OrderedDict
 
+import base64
 import cv2
 import numpy as np
 
-__all__ = ['postprocess']
+__all__ = ['base64_to_cv2', 'postprocess']
+
+
+def base64_to_cv2(b64str):
+    data = base64.b64decode(b64str.encode('utf8'))
+    data = np.fromstring(data, np.uint8)
+    data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    return data
 
 
 def postprocess(data_out,
